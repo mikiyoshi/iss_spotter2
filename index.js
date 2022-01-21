5e2833e69dbe3ee74483b1ce53a678cf79cdca98
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-01-21 01:17:50 +0000

    ISS Spotter II

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // index.js
 
 // The code below is temporary and can be commented out.
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:', ip);
+// });
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP(
+  '2607:fea8:520:a7c0:bce9:5817:dba7:45ef',
+  (error, coordinates) => {
+    if (error) {
+      console.log("It didn't work!", error);
+      return;
+    }
+
+    console.log('It worked! Returned coordinates:', coordinates);
   }
-
-  console.log('It worked! Returned IP:', ip);
-});
+);
